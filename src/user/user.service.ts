382424819baa1d28fb1ac15b9aed9a90e0c42f43
ebagabe <gabriel.ebagabe@gmail.com>
2024-03-01 00:12:33 -0300

    Pause break: migration

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,29 +1,29 @@
 import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/createUser.dto";
-import { User } from "./interface/user.interface";
+import { UserEntity } from "./interface/user.entity";
 import { hash } from "bcrypt";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class UserService {
-  private users: User[] = [];
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
-  async CreateUser(createUserDto: CreateUserDto): Promise<User> {
+  async CreateUser(createUserDto: CreateUserDto): Promise<UserEntity> {
     const saltOrRounds = 10;
 
     const passwordHash = await hash(createUserDto.password, saltOrRounds);
 
-    const user: User = {
+    return this.userRepository.save({
       ...createUserDto,
-      id: this.users.length + 1,
       password: passwordHash,
-    };
-
-    this.users.push(user);
-
-    return user;
+    });
   }
 
-  async getAllUser(): Promise<User[]> {
-    return this.users;
+  async getAllUser(): Promise<UserEntity[]> {
+    return this.userRepository.find();
   }
 }
